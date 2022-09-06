10ae444cc12e67ecbb86b5213927785aa589acab
faratim <104940465+faratim@users.noreply.github.com>
2022-09-06 15:38:58 -0400

    initiated project from class comps file
    Changing project from class components to functional components.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
+
+const App = () => {
+    return(<div className='App'>
+    <h1 className="app-title">Monsters Rolodex</h1> 
+     
+     <SearchBox
+         className='monsters-search-box'
+         onChangeHandler={onSearchChange}
+         placeholder='search monsters' />
+     <CardList monsters={filteredMonsters} />
+ </div>)
+}
 class App extends Component {
     constructor() {
         super();
